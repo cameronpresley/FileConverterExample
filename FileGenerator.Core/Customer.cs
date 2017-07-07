27289d5c53e975b45bca5bf18554354196059420
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-06 21:17:41 -0400

    Added Core project with a Record object.

---
+++ FileGenerator.Core/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FileGenerator.Core
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+    }
+
+}
